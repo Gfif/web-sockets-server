4c4a75fa99b92d6b5e8258c62d965e97ebc20ba3
Pavel Sviridov <p.y.sviridov@gmail.com>
2015-09-23 00:36:58 +0600

    Implement heartbeat

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
@@ -9,6 +10,22 @@ import (
 )
 
 var X int64 = 1000
+
+func readHeartbeat(conn *websocket.Conn) chan int {
+	resChan := make(chan int)
+	go func() {
+		for {
+			_, _, err := conn.NextReader()
+			if err != nil {
+				log.WithError(err).Warn("heartbeat is lost")
+				return
+			}
+		}
+		resChan <- 1
+	}()
+
+	return resChan
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	upgrader := websocket.Upgrader{
@@ -31,17 +48,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 	}()
 
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.WithError(err).Error("Failed to read message")
-			return
-		}
-		if err = conn.WriteMessage(messageType, p); err != nil {
-			log.WithError(err).Error("Failed to write message")
-			return
-		}
+	hbChan := readHeartbeat(conn)
+	select {
+	case <-hbChan:
+		log.Info("Heartbeat recieved")
+	case <-time.After(time.Millisecond * 3 * time.Duration(X)):
+		log.Error("Timeout")
+		return
 	}
+
+	// for _ = range time.NewTicker(time.Second * 10).C {
+	// 	if err = conn.WriteMessage(1, []byte("Some message")); err != nil {
+	// 		log.WithError(err).Error("Failed to write message")
+	// 		return
+	// 	}
+	// }
 }
 
 func main() {
